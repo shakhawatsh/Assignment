ffafc2c417103b5f87456f10dce9114d6105e9dd
shakhawatsh <shakhawatshuvo1@gmail.com>
2024-03-10 03:47:48 +0600

    Initial commit

---
+++ Assignment.dart
@@ -0,0 +1,25 @@
+class Media{
+    play(){
+    print('Playing media...');
+  }
+
+}
+
+class Song implements Media{
+
+  String artist='Metallica';
+
+  @override
+  play() {
+    print('Playing song by $artist');
+  }
+
+}
+
+void main(){
+  var MediaPlay=Media();
+  print(MediaPlay.play());
+  var SongPlay= Song();
+ print(SongPlay.play());
+
+}
